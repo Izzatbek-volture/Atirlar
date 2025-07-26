39d544575b5bddb4c29c16c9808cd28d92f8c91f
Izzatbek-volture <ibragimovrustamjon247@gmail.com>
2025-07-26 11:47:01 +0500

    atir

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Katalog from './Katalog'
+import Hero from './Hero'
+import Footer from 'Footer'
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Katalog />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
