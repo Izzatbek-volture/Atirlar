2dd5ab729fb3fd0834e7f87e7b1a7ca82b441afc
Izzatbek-volture <ibragimovrustamjon247@gmail.com>
2025-07-26 11:53:13 +0500

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Navbar from './Navbar'
 import Katalog from './Katalog'
 import Hero from './Hero'
-import Footer from 'Footer'
+import Footer from './Footer'
 export default function App() {
   return (
     <>
       <Navbar />
-      <Katalog />
       <Hero />
+      <Katalog />
       <Footer />
     </>
   )
